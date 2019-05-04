d804af94f26a7c05b76dc84c6e4ccb59d5b253b5
wangwenbo <765912435@qq.com>
2019-05-04 21:02:22 +0800

    header的布局

---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="header">
+    <div class="header-left">返回</div>
+    <div class="header-center">输入城市/景点/游玩主题</div>
+    <div class="header-right">城市</div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+<style lang="stylus" scoped>
+  .header
+    display flex
+    line-height .86rem
+    background-color #00bcd4
+    color #ffffff
+    .header-left
+      width .64rem
+      float left
+    .header-center
+      flex 1
+      height .64rem
+      line-height .64rem
+      background-color #ffffff
+      border-radius .1rem
+      margin-top .12rem
+      margin-left .12rem
+      color #ccc
+    .header-right
+      width 1.24rem
+      float right
+      text-align center
+</style>
