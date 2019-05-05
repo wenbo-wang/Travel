f8bc8cd69a9b4fd18c7f01665ce79ebbc1731e74
wangwenbo <765912435@qq.com>
2019-05-05 16:38:28 +0800

    完成热销title的布局

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -3,18 +3,21 @@
     <home-header></home-header>
     <home-swiper></home-swiper>
     <home-icons></home-icons>
+    <home-hot-sale></home-hot-sale>
   </div>
 </template>
 <script>
 import HomeHeader from './components/Header'
 import HomeSwiper from './components/Swiper'
 import HomeIcons from './components/Icons'
+import HomeHotSale from './components/HotSale'
 export default {
   name: 'Home',
   components: {
     HomeHeader,
     HomeSwiper,
-    HomeIcons
+    HomeIcons,
+    HomeHotSale
   }
 }
 </script>
---
+++ src/pages/home/components/HotSale.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="hot">
+    <div class="hot-title">
+      <img class="hot-title-img" src="http://img1.qunarzz.com/piao/fusion/1711/16/bfbb9874e8f11402.png">
+      <span class="hot-title-desc">本周热门榜单</span>
+      <div class="hot-title-all">
+        <span>全部榜单</span>
+        <span class="iconfont">&#xe6f3;</span>
+      </div>
+    </div>
+    <ul class="hot-list">
+      <li class="hot-list-item">ceshi</li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HomeHotSale'
+}
+</script>
+<style lang="stylus" scoped>
+  .hot
+    height  3.6rem
+    .hot-title
+      position relative
+      height .44rem
+      padding .24rem 0 .26rem
+      .hot-title-img
+        float left
+        display block
+        width .3rem
+        height .3rem
+        margin-left .2rem
+      .hot-title-desc
+        float left
+        font-size .32rem
+        height .44rem
+        line-height .44rem
+        margin-left .08rem
+        color: #212121
+      .hot-title-all
+        float right
+    .hot-list
+      overflow-x scroll
+      padding 0 .24rem
+      .hot-list-item
+        float left
+        width 2rem
+</style>
