372413ca02bbcac9be82cc07a2819ee31c825bf8
wangwenbo <765912435@qq.com>
2019-05-05 21:15:12 +0800

    数据抽取

--- src/pages/home/components/HotSale.vue
+++ src/pages/home/components/HotSale.vue
@@ -9,114 +9,18 @@
       </div>
     </div>
     <ul class="hot-list">
-      <li class="hot-list-item">
+      <li class="hot-list-item" v-for="item of hotList" :key="item.id">
         <div class="hot-list-item-full">
           <div class="item-full-tag">
-            <img class="item-full-tag-img" src="http://img1.qunarzz.com/piao/fusion/1710/ab/159673b63e6ca702.png">
+            <img class="item-full-tag-img" :src="item.tagUrl">
           </div>
           <div class="item-full-img">
-            <img class="full-img-icon" src="http://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_250x250_0fc722c0.jpg">
+            <img class="full-img-icon" :src="item.imgUrl">
           </div>
-          <div class="item-full-desc">故宫</div>
+          <div class="item-full-desc">{{item.desc}}</div>
           <div class="item-full-price">
             ￥
-            <em class="price-num">60</em>
-            起
-          </div>
-        </div>
-      </li>
-      <li class="hot-list-item">
-        <div class="hot-list-item-full">
-          <div class="item-full-tag">
-            <img class="item-full-tag-img" src="http://img1.qunarzz.com/piao/fusion/1710/ab/159673b63e6ca702.png">
-          </div>
-          <div class="item-full-img">
-            <img class="full-img-icon" src="http://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_250x250_0fc722c0.jpg">
-          </div>
-          <div class="item-full-desc">故宫</div>
-          <div class="item-full-price">
-            ￥
-            <em class="price-num">60</em>
-            起
-          </div>
-        </div>
-      </li>
-      <li class="hot-list-item">
-        <div class="hot-list-item-full">
-          <div class="item-full-tag">
-            <img class="item-full-tag-img" src="http://img1.qunarzz.com/piao/fusion/1710/ab/159673b63e6ca702.png">
-          </div>
-          <div class="item-full-img">
-            <img class="full-img-icon" src="http://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_250x250_0fc722c0.jpg">
-          </div>
-          <div class="item-full-desc">故宫</div>
-          <div class="item-full-price">
-            ￥
-            <em class="price-num">60</em>
-            起
-          </div>
-        </div>
-      </li>
-      <li class="hot-list-item">
-        <div class="hot-list-item-full">
-          <div class="item-full-tag">
-            <img class="item-full-tag-img" src="http://img1.qunarzz.com/piao/fusion/1710/ab/159673b63e6ca702.png">
-          </div>
-          <div class="item-full-img">
-            <img class="full-img-icon" src="http://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_250x250_0fc722c0.jpg">
-          </div>
-          <div class="item-full-desc">故宫</div>
-          <div class="item-full-price">
-            ￥
-            <em class="price-num">60</em>
-            起
-          </div>
-        </div>
-      </li>
-      <li class="hot-list-item">
-        <div class="hot-list-item-full">
-          <div class="item-full-tag">
-            <img class="item-full-tag-img" src="http://img1.qunarzz.com/piao/fusion/1710/ab/159673b63e6ca702.png">
-          </div>
-          <div class="item-full-img">
-            <img class="full-img-icon" src="http://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_250x250_0fc722c0.jpg">
-          </div>
-          <div class="item-full-desc">故宫</div>
-          <div class="item-full-price">
-            ￥
-            <em class="price-num">60</em>
-            起
-          </div>
-        </div>
-      </li>
-      <li class="hot-list-item">
-        <div class="hot-list-item-full">
-          <div class="item-full-tag">
-            <img class="item-full-tag-img" src="http://img1.qunarzz.com/piao/fusion/1710/ab/159673b63e6ca702.png">
-          </div>
-          <div class="item-full-img">
-            <img class="full-img-icon" src="http://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_250x250_0fc722c0.jpg">
-          </div>
-          <div class="item-full-desc">故宫</div>
-          <div class="item-full-price">
-            ￥
-            <em class="price-num">60</em>
-            起
-          </div>
-        </div>
-      </li>
-      <li class="hot-list-item">
-        <div class="hot-list-item-full">
-          <div class="item-full-tag">
-            <img class="item-full-tag-img" src="http://img1.qunarzz.com/piao/fusion/1710/ab/159673b63e6ca702.png">
-          </div>
-          <div class="item-full-img">
-            <img class="full-img-icon" src="http://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_250x250_0fc722c0.jpg">
-          </div>
-          <div class="item-full-desc">故宫</div>
-          <div class="item-full-price">
-            ￥
-            <em class="price-num">60</em>
+            <em class="price-num">{{item.price}}</em>
             起
           </div>
         </div>
@@ -126,7 +30,54 @@
 </template>
 <script>
 export default {
-  name: 'HomeHotSale'
+  name: 'HomeHotSale',
+  data () {
+    return {
+      hotList: [{
+        id: '0001',
+        tagUrl: 'http://img1.qunarzz.com/piao/fusion/1710/ab/159673b63e6ca702.png',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_250x250_0fc722c0.jpg',
+        desc: '故宫',
+        price: '60'
+      }, {
+        id: '0002',
+        tagUrl: 'http://img1.qunarzz.com/piao/fusion/1710/ab/159673b63e6ca702.png',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_250x250_0fc722c0.jpg',
+        desc: '长城',
+        price: '70'
+      }, {
+        id: '0003',
+        tagUrl: 'http://img1.qunarzz.com/piao/fusion/1710/ab/159673b63e6ca702.png',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_250x250_0fc722c0.jpg',
+        desc: '长城',
+        price: '80'
+      }, {
+        id: '0004',
+        tagUrl: 'http://img1.qunarzz.com/piao/fusion/1710/ab/159673b63e6ca702.png',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_250x250_0fc722c0.jpg',
+        desc: '长城',
+        price: '90'
+      }, {
+        id: '0005',
+        tagUrl: 'http://img1.qunarzz.com/piao/fusion/1710/ab/159673b63e6ca702.png',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_250x250_0fc722c0.jpg',
+        desc: '长城',
+        price: '100'
+      }, {
+        id: '0006',
+        tagUrl: 'http://img1.qunarzz.com/piao/fusion/1710/ab/159673b63e6ca702.png',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_250x250_0fc722c0.jpg',
+        desc: '长城',
+        price: '110'
+      }, {
+        id: '0007',
+        tagUrl: 'http://img1.qunarzz.com/piao/fusion/1710/ab/159673b63e6ca702.png',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_250x250_0fc722c0.jpg',
+        desc: '长城',
+        price: '120'
+      }]
+    }
+  }
 }
 </script>
 <style lang="stylus" scoped>
@@ -156,6 +107,7 @@ export default {
     .hot-list-item
       display: inline-block
       width 2rem
+      margin-left .1rem
       margin-bottom .2rem
       .hot-list-item-full
         width 100%
