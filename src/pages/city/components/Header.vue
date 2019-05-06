41d537035716c999776b54c5a6cdc37565f8e21b
wangwenbo <765912435@qq.com>
2019-05-06 22:03:40 +0800

    完成city header router

---
+++ src/pages/city/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="header">
+    <router-link to="/">
+      <span class="iconfont back-icon">&#xe6f5;</span>
+    </router-link>
+    <h1 class="header-title">城市选择</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: 'CityHeader'
+}
+</script>
+<style lang="stylus" scoped>
+  @import "~styles/varibles.styl"
+  .header
+    position relative
+    line-height $headerHeight
+    background-color $bgColor
+    .back-icon
+      position absolute
+      top 0
+      left 0
+      font-size .4rem
+      color #ffffff
+    .header-title
+      text-align center
+      font-size .34rem
+      color #ffffff
+</style>
