acadf27b352b55f37ab5ee4572b6a3898abc2fe3
wangwenbo <765912435@qq.com>
2019-05-08 15:24:31 +0800

    实现了search组件的数据逻辑

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -1,26 +1,90 @@
 <template>
   <div class="search">
-    <input class="search-input" type="text" placeholder="输入城市名或拼音">
+    <div class="input-wrapper">
+      <input class="search-input" type="text" placeholder="输入城市名或拼音" v-model="keywords">
+    </div>
+    <div class="search-content" ref="content" v-show="this.keywords">
+      <ul>
+        <li class="search-item border-bottom" v-for="item of list" :key="item.id">{{item.name}}</li>
+        <li class="search-item border-bottom" v-show="hasData">没有匹配到结果</li>
+      </ul>
+    </div>
   </div>
 </template>
 <script>
+import Bscroll from 'better-scroll'
 export default {
-  name: 'CitySearch'
+  name: 'CitySearch',
+  props: {
+    cities: Object
+  },
+  data () {
+    return {
+      keywords: '',
+      list: [],
+      timer: null
+    }
+  },
+  computed: {
+    hasData () {
+      return !this.list.length
+    }
+  },
+  watch: {
+    keywords () {
+      if (this.timer) {
+        clearTimeout(this.timer)
+      }
+      if (!this.keywords) {
+        this.list = []
+        return
+      }
+      this.timer = setTimeout(() => {
+        const result = []
+        for (let i in this.cities) {
+          this.cities[i].forEach((value) => {
+            if (value.name.indexOf(this.keywords) > -1 || value.spell.indexOf(this.keywords) > -1) {
+              result.push(value)
+            }
+          })
+        }
+        this.list = result
+      })
+    }
+  },
+  mounted () {
+    console.log(this.$refs.content)
+    this.scroll = new Bscroll(this.$refs.content)
+  }
 }
 </script>
 <style lang="stylus" scoped>
   @import "~styles/varibles.styl"
   .search
-    height .72rem
-    padding 0 .1rem
-    background-color $bgColor
-    .search-input
-      box-sizing border-box
-      width 100%
-      height .62rem
-      line-height .62rem
-      border-radius .1rem
-      text-align center
-      color #666
+    .input-wrapper
+      height .72rem
       padding 0 .1rem
+      background-color $bgColor
+      .search-input
+        box-sizing border-box
+        width 100%
+        height .62rem
+        line-height .62rem
+        border-radius .1rem
+        text-align center
+        color #666
+        padding 0 .1rem
+    .search-content
+      overflow hidden
+      z-index 1
+      position absolute;
+      top 1.56rem
+      left 0
+      right 0
+      bottom 0
+      background-color #eee
+      .search-item
+        line-height .62rem
+        padding-left .2rem
+        background-color #ffffff
 </style>
